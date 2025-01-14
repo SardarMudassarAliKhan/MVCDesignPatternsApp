dafcf4184ae3af2fd26f80ea543aaba14f46a487
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2025-01-15 02:14:08 +0500

    Add UnitOfWork and Product classes
    Introduce UnitOfWork class in MVCDesignPatternsApp.AppDbContext namespace to manage ApplicationDbContext lifecycle and repositories. Implement IDisposable interface. Add Product class in MVCDesignPatternsApp.Models namespace with Id, Name, and Price properties.

---
+++ AppDbContext/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using MVCDesignPatternsApp.AppContext;
+using MVCDesignPatternsApp.AppRepository;
+using MVCDesignPatternsApp.IAppRepository;
+using MVCDesignPatternsApp.Models;
+
+namespace MVCDesignPatternsApp.AppDbContext
+{
+    public class UnitOfWork : IDisposable
+    {
+        private readonly ApplicationDbContext _context;
+        private IRepository<Product> _productRepository;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<Product> ProductRepository
+        {
+            get
+            {
+                return _productRepository ??= new Repository<Product>(_context);
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
